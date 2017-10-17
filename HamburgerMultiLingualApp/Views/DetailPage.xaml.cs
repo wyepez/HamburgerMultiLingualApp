8139a78627f7818f433e7bda08763c286852c2c9
Washington Yepez <w.yepez@ottcomputer.ec>
2017-10-17 18:34:39 +0200

    Add project files.

---
+++ HamburgerMultiLingualApp/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using HamburgerMultiLingualApp.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HamburgerMultiLingualApp.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
